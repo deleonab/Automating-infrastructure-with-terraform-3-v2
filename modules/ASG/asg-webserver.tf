a1de263d91ddc269aee402b756a6fc922dca2df8
Dele Onabowu <delesodade@hotmail.com>
2022-08-08 23:01:20 +0100

    Refactored files into modules

--- modules/ASG/asg-webserver.tf
+++ modules/ASG/asg-webserver.tf
@@ -38,16 +38,12 @@ resource "aws_launch_template" "wordpress-launch-template" {
 
 resource "aws_autoscaling_group" "wordpress-asg" {
   name                      = "wordpress-asg"
-  max_size                  = 2
-  min_size                  = 2
+  max_size                  = var.max_size
+  min_size                  = var.min_size
   health_check_grace_period = 300
   health_check_type         = "ELB"
-  desired_capacity          = 2
-  vpc_zone_identifier = [
-
-    aws_subnet.private[0].id,
-    aws_subnet.private[1].id
-  ]
+  desired_capacity          = var.desired_capacity
+  vpc_zone_identifier = var.private_subnets 
 
   launch_template {
     id      = aws_launch_template.wordpress-launch-template.id
@@ -63,7 +59,7 @@ resource "aws_autoscaling_group" "wordpress-asg" {
 # attaching autoscaling group of  wordpress application to internal loadbalancer
 resource "aws_autoscaling_attachment" "asg_attachment_wordpress" {
   autoscaling_group_name = aws_autoscaling_group.wordpress-asg.id
-  lb_target_group_arn   = aws_lb_target_group.wordpress-tgt.arn
+  lb_target_group_arn   = var.wordpress-alb-tgt
 }
 
 # launch template for toooling
@@ -111,7 +107,7 @@ resource "aws_autoscaling_group" "tooling-asg" {
   health_check_type         = "ELB"
   desired_capacity          = var.desired_capacity
 
-  vpc_zone_identifier = [var.private_subnets]
+  vpc_zone_identifier = var.private_subnets
 
   launch_template {
     id      = aws_launch_template.tooling-launch-template.id
