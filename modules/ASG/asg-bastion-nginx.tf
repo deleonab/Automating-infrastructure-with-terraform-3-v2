dc50874771d0c801143d71b7b04858a9ac9708f9
Dele Onabowu <delesodade@hotmail.com>
2022-08-09 20:09:40 +0100

    Refactored files into modules

--- modules/ASG/asg-bastion-nginx.tf
+++ modules/ASG/asg-bastion-nginx.tf
@@ -20,44 +20,10 @@ resource "aws_autoscaling_notification" "dele_notifications" {
   topic_arn = aws_sns_topic.dele-sns.arn
 }
 
-
-# launch template for bastion
 resource "random_shuffle" "az_list" {
   input        = data.aws_availability_zones.available.names
 }
 
-resource "aws_launch_template" "bastion-launch-template" {
-  image_id               = var.ami-bastion
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = var.bastion-sg
-
-  iam_instance_profile {
-    name = var.instance_profile
-  }
-
-  key_name = var.keypair
-
-  placement {
-    availability_zone = "random_shuffle.az_list.result"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-
-   tags = merge(
-    var.tags,
-    {
-      Name = "bastion-launch-template"
-    },
-  )
-  }
-
-  user_data = filebase64("${path.module}/bastion.sh")
-}
 
 # ---- Autoscaling for bastion  hosts
 
@@ -83,40 +49,7 @@ resource "aws_autoscaling_group" "bastion-asg" {
 
 }
 
-# launch template for nginx
 
-resource "aws_launch_template" "nginx-launch-template" {
-  image_id               = var.ami-nginx
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = var.nginx-sg
-
-  iam_instance_profile {
-    name = aws_iam_instance_profile.ip.id
-  }
-
-  key_name =  var.keypair
-
-  placement {
-    availability_zone = "random_shuffle.az_list.result"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag_specifications {
-    resource_type = "instance"
-
-    tags = merge(
-    var.tags,
-    {
-      Name = "nginx-launch-template"
-    },
-  )
-  }
-
-  user_data = filebase64("${path.module}/nginx.sh")
-}
 
 # ------ Autoscslaling group for reverse proxy nginx ---------
 
