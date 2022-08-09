7a4d474efc6b1f9bae54d72d1b879529be691392
Dele Onabowu <delesodade@hotmail.com>
2022-08-09 22:01:31 +0100

    Refactored files into modules

---
+++ modules/VPC/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "enable_dns_support" {
+  type = bool
+}
+
+variable "enable_dns_hostnames" {
+  type = bool
+}
+
+variable "enable_classiclink" {
+  type = bool
+}
+
+
